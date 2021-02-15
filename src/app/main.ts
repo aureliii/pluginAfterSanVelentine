6da976751b18c01a8a963e07f9f4f62767f7c399
apiersanti <alessandro.piersanti@soprasteria.com>
2021-02-15 16:55:25 +0100

    retrievers

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,17 +1,28 @@
 import  retriveObjName  from "./retriveObjName";
 import  retriveUserPermissionName  from "./retriveUserPermissionName";
-
+import profileRetriever from "./profileRetriever";
+import * as sfcore from '@salesforce/core/lib/connection';
+import * as sfmeta from '@Types/jsforce/api/metadata';
 
 export default class main {
-    public static async  start(conn){
+    public static async  start(conn : sfcore.Connection){
     
-     console.log('sono nel main');
-     var objectsName = await retriveObjName.getObjsName(conn);
-     var userPermissionName = await retriveUserPermissionName.getNames(conn);
-     console.log('objectsName',objectsName);
-     console.log('userPermissionName',userPermissionName);
-       
-   };
-   
- }
- 
+    	console.log('sono nel main');
+	 	var objectsName = await retriveObjName.getObjsName(conn);
+     	let userPermissionName = await retriveUserPermissionName.retrieveUserPermissions(conn);
+     	console.log('objectsName',objectsName);
+     	console.log('userPermissionName', userPermissionName);
+
+		let profileNames = await profileRetriever.retrieveProfileNames(conn);
+
+		var profileList = new Map<String, sfmeta.MetadataInfo>();
+		var i, j, tempArray, chunk = 10;
+        for (i=0, j=profileNames.length; i<j; i += chunk) {
+			tempArray = profileNames.slice(i,i+chunk);
+			console.log("temArray len: " + tempArray.length);
+            await profileRetriever.retrieveProfile(conn, tempArray, profileList);
+            console.log("profileList len: " + profileList.size);
+		}
+		console.log('Profiles: ', profileList);
+   	};
+}
--- src/app/retriveUserPermissionName.ts
+++ src/app/retriveUserPermissionName.ts
@@ -1,25 +1,17 @@
-
-
-
+import * as sfcore from '@salesforce/core/lib/connection';
 
 export default class retriveUserPermissionName{ 
-    public static async  getNames(conn) {
-       try{
-        let permissionName = [];
-        await conn.sobject("UserPermissionAccess").describe(function(err, metadata){
-            if(err){ return console.error(err); }
-            console.log('Label: ' + metadata.label);
-            console.log('Number of Fields: ' + metadata.fields.length);
-            metadata.fields.forEach(element => {
-                if(element.name.startsWith('Permissions'))
-                    permissionName.push(element.name.replace('Permissions', ''));
-            });
+
+    public static async retrieveUserPermissions(conn : sfcore.Connection) {
+        let permissionName: string[] = new Array<string>();
+        const describeResult = await conn.describe('UserPermissionAccess');
+
+        await describeResult.fields.forEach(element => {
+            if(element.name.startsWith('Permissions'))
+                permissionName.push(element.name.replace('Permissions', ''));
         });
+
         console.log('All permissionName: ', permissionName);
         return permissionName;
-       }catch(err) {
-        console.error(err);
-       } 
-        
-    };
+    }
 }
