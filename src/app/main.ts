196cfcb6184bdf2f5111c40f55c080309b51fc54
amrruku <aurel.mrruku@fastweb.it>
2021-02-16 12:07:03 +0100

    ref

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,8 +2,8 @@ import  retriveObjName  from "./retriveObjName";
 import  retriveUserPermissionName  from "./retriveUserPermissionName";
 import profileRetriever from "./profileRetriever";
 import tabFix from "./tabFix";
-import userPermissionFix from "./userPermissionFix";
-import objectPermissionFix from "./objectPermissionFix";
+// import userPermissionFix from "./userPermissionFix";
+// import objectPermissionFix from "./objectPermissionFix";
 import * as sfcore from '@salesforce/core/lib/connection';
 import writeprofile from "./writeprofile";
 // import * as sfmeta from '@Types/jsforce/api/metadata';
@@ -18,10 +18,11 @@ export default class main {
      	console.log('userPermissionName', userPermissionName);
 
 		let profileMtd = await profileRetriever.retriveProfileMTD(conn);
-		profileMtd = await writeprofile.write(profileMtd);
 		profileMtd = await tabFix.fix(profileMtd);
-		profileMtd = await userPermissionFix.fix(profileMtd,userPermissionName);
-		profileMtd = await objectPermissionFix.fix(profileMtd,objectsName);
+		profileMtd = await writeprofile.write(profileMtd);
+		
+	//	profileMtd = await userPermissionFix.fix(profileMtd,userPermissionName);
+	//	profileMtd = await objectPermissionFix.fix(profileMtd,objectsName);
 		// profileMtd = await writeprofile.write(profileMtd);
 
 		
