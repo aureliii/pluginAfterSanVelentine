7e85ebb5e18a19e64481880f53ccd3fd651625b9
amrruku <aurel.mrruku@fastweb.it>
2021-02-16 11:43:28 +0100

    ref

--- src/app/main.ts
+++ src/app/main.ts
@@ -18,10 +18,11 @@ export default class main {
      	console.log('userPermissionName', userPermissionName);
 
 		let profileMtd = await profileRetriever.retriveProfileMTD(conn);
+		profileMtd = await writeprofile.write(profileMtd);
 		profileMtd = await tabFix.fix(profileMtd);
 		profileMtd = await userPermissionFix.fix(profileMtd,userPermissionName);
 		profileMtd = await objectPermissionFix.fix(profileMtd,objectsName);
-		profileMtd = await writeprofile.write(profileMtd);
+		// profileMtd = await writeprofile.write(profileMtd);
 
 		
 
