abe27a81b8633b63f585e845f2ffe10e8522e534
amrruku <aurel.mrruku@fastweb.it>
2021-02-17 15:11:42 +0100

    ref

--- src/app/objectPermissionFix.ts
+++ src/app/objectPermissionFix.ts
@@ -10,7 +10,7 @@ export default class objectPermissionFix{
                           if (!objectNames.includes(metadataProfile.objectPermissions.object)) {
                             objectNames.push(metadataProfile.objectPermissions.object);
                           }             
-                        //metadataProfile.objectPermissions = Object.entries(metadataProfile.objectPermissions);
+                           metadataProfile.objectPermissions = Object.entries(metadataProfile.objectPermissions);
                     } else {
                           for(var permObject of metadataProfile.objectPermissions){
                             if ( permObject  !== null && permObject.object !== null  && !objectNames.includes(permObject.object)) {
